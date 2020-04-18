1336b26eb2dc6d5638ad374fb7fe7aefb3e77748
Tami Shute <tamaralynnrenee@gmail.com>
2020-04-18 17:42:04 -0400

    initialize files from activities

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Carls');
+INSERT INTO burgers (name) VALUES ('Rodeo');
+INSERT INTO burgers (name, eaten) VALUES ('Roadhouse', true);
+INSERT INTO burgers (name, eaten) VALUES ('Good Morning Sunshine', true);
+INSERT INTO burgers (name, eaten) VALUES ('Monster', true);
+INSERT INTO burgers (name) VALUES ('Bacon Bleu');
